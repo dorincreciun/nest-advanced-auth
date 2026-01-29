fe0eef4ff920cc63abce86e131f6e4f48c3a3b49
Dorin <creciundorin25@gmail.com>
2026-01-29 12:24:48 +0200

    feat(database): create database module and added connection

---
+++ src/modules/database/database.module.ts
@@ -0,0 +1,27 @@
+import {Global, Module} from "@nestjs/common";
+import {ConfigService} from "@nestjs/config";
+import {EnvironmentVariables} from "../../common/types/environment-variables.types";
+import {Pool} from "pg";
+import {PG_CONNECTION} from "../../common/constants/providers-keys.constants";
+
+@Global()
+@Module({
+    providers: [
+        {
+            provide: PG_CONNECTION,
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService<EnvironmentVariables>) => {
+                return new Pool({
+                    host: configService.get('DB_HOST'),
+                    port: configService.get('DB_PORT'),
+                    user: configService.get('DB_USER'),
+                    database: configService.get('DB_NAME'),
+                    password: configService.get('DB_PASS'),
+                })
+            }
+        },
+    ],
+    exports: [PG_CONNECTION],
+})
+
+export class DatabaseModule {}
